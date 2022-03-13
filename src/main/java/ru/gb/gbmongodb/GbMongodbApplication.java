efc23d70dccf0481eb831ba9a03422bc61cdf062
W153 <DonVitalko@mail.ru>
2022-03-13 04:27:48 +0300

    Initial commit

---
+++ src/main/java/ru/gb/gbmongodb/GbMongodbApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gbmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/gbmongodb/GbMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
